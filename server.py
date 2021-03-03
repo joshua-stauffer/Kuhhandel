911f645eca403c959eeecea932ca0a8a6a01d341
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2021-03-03 01:46:08 -0500

    server handles multiple games

--- server.py
+++ server.py
@@ -5,13 +5,28 @@ from uuid import uuid4
 from game.game import Game
 from game.player import Player
 
-game = Game(auction_timeout=15)
+
+current_games = []
+
+def get_game():
+    """Returns a game with open spots"""
+
+    if not len(current_games) or current_games[-1].is_ready:
+        game = Game(auction_timeout=15)
+        current_games.append(game)
+        loop = asyncio.get_running_loop()
+        asyncio.create_task(game.run())
+        return game
+    else:
+        print('joining last game of list')
+        return current_games[-1]
 
 async def lobby(websocket, path):
-    """Entry point for client to join game"""
+    """Entry point for a client to join game"""
 
+    game = get_game()
     player = Player(websocket, str(uuid4()))
-    task = asyncio.create_task(player.client.handle_msgs(game.is_complete))
+    sock_handler = asyncio.create_task(player.client.handle_msgs(game.is_complete))
     await player.get_name()
     game.add_player(player)
     await player.client.send_msg(
@@ -21,12 +36,20 @@ async def lobby(websocket, path):
     
     # when this function returns, the player websocket is closed
     while not game.is_complete:
-        await asyncio.sleep(1)
+        await asyncio.sleep(10)
 
+async def cleanup_games():
+    """Check currently running games for completed games and remove them"""
+
+    while True:
+        for game in current_games:
+            if game.is_complete:
+                current_games.remove(game)
+        await asyncio.sleep(60)
+
+# create the loop and server
 start_server = websockets.serve(lobby, 'localhost', 9876)
-
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_server)
 print('started server at port 9876')
-loop.create_task(game.run())
 loop.run_forever()
